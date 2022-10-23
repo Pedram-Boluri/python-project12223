e4290fe66667938ca0952916b50e6437b030c96b
Pedram-Boluri <115915558+Pedram-Boluri@users.noreply.github.com>
2022-10-23 20:22:29 +0330

    Add files via upload

---
+++ pre ex 1.py
@@ -0,0 +1,195 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def Ex1_example(num_minutes):
+    '''
+ (int) -> int
+
+    This function will convert the given number of minutes into seconds.
+
+    example:
+    >>> Ex1_example(12)
+    720
+
+    '''
+    seconds= num_minutes*60
+    return(seconds)
+Ex1_example(12)
+SyntaxError: invalid syntax
+help(ex1_example)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    help(ex1_example)
+NameError: name 'ex1_example' is not defined
+ Ex1_example(12)
+ 
+SyntaxError: unexpected indent
+Ex1_example(12)=
+SyntaxError: cannot assign to function call
+ef Ex1_example(num_minutes):
+    '''
+ (int) -> int
+
+    This function will convert the given number of minutes into seconds.
+
+    example:
+    >>> Ex1_example(12)
+    720
+
+    '''
+    seconds = num_minutes*60
+    return(seconds)
+SyntaxError: invalid syntax
+def Ex1_example(num_minutes):
+    '''
+ (int) -> int
+
+    This function will convert the given number of minutes into seconds.
+
+    example:
+    >>> Ex1_example(12)
+    720
+
+    '''
+    seconds = num_minutes*60
+    return(seconds)
+Ex1 (12)
+SyntaxError: invalid syntax
+
+EX1_example(12)=
+SyntaxError: cannot assign to function call
+SyntaxError: cannot assign to function call
+SyntaxError: invalid syntax
+def min2sec(num_min)
+SyntaxError: expected ':'
+ def min2sec(num_mine):
+     
+SyntaxError: unexpected indent
+def min2sec(num_minutes):
+    '''
+ (int) -> int
+ '''
+    seconds= num_minutes*60
+    return(seconds)
+
+
+Ex1(12)
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    Ex1(12)
+NameError: name 'Ex1' is not defined
+Ex1_example(12)
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    Ex1_example(12)
+NameError: name 'Ex1_example' is not defined
+Ex1_example(minutes)
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    Ex1_example(minutes)
+NameError: name 'Ex1_example' is not defined
+def Ex1_example(minutes)
+SyntaxError: expected ':'
+def Ex1_example(minutes):
+    print(minutes*60)
+
+    
+Ex1_example(12)
+720
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Ex1_1(number):
+    print(number+1)
+
+    
+Ex1_1(12)
+13
+
+
+
+def Ex1_2(a,h)
+SyntaxError: expected ':'
+def Ex1_2(a,h):
+    print(a*h/2)
+
+    
+
+Ex1_2(3,2)
+3.0
+def Ex1_3(side1,side2):
+    print('the maximum range of the 3rd side is less than' side1+sided2)
+    
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+def Ex1_3(side1,side2):
+...     print('The maximum range of the 3rd side is less than' {}.format(side1+sided2))
+...     
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> def EX1_3(side1,side2):
+...     txt = 'The third side of this triangle can not be equal or more than {}'
+...     print(txt.format(side1+side2))
+... 
+...     
+>>> Ex1_3(1,2)
+Traceback (most recent call last):
+  File "<pyshell#83>", line 1, in <module>
+    Ex1_3(1,2)
+NameError: name 'Ex1_3' is not defined. Did you mean: 'EX1_3'?
+>>> EX1_3(1,2)
+The third side of this triangle can not be equal or more than 3
+>>> def EX1_3(side1,side2):
+...     txt = "the third side is  > {}'
+...     
+SyntaxError: unterminated string literal (detected at line 2)
+>>> ef EX1_3(side1,side2):
+...     txt = "the third side is  > {}"
+...     
+SyntaxError: invalid syntax
+>>> def EX1_3(side1,side2):
+...     txt = "the third side is  > {}"
+...     print(text.format(side1+side2))
+... 
+...     
+>>> EX1_3(1,2)
+Traceback (most recent call last):
+  File "<pyshell#90>", line 1, in <module>
+    EX1_3(1,2)
+  File "<pyshell#89>", line 3, in EX1_3
+    print(text.format(side1+side2))
+NameError: name 'text' is not defined. Did you mean: 'txt'?
+>>> def EX1_3(side1,side2):
+...     txt = "the third side is  > {}"
+...     print(txt.format(side1+side2))
+... 
+...     
+>>> EXX1_3(1,2)
+Traceback (most recent call last):
+  File "<pyshell#93>", line 1, in <module>
+    EXX1_3(1,2)
+NameError: name 'EXX1_3' is not defined. Did you mean: 'EX1_3'?
+>>> EX1_3(1,2)
+the third side is  > 3
